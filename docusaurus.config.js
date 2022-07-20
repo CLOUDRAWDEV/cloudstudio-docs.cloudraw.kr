10e1e477b9eec252d42db215c4ff78d17c9f05c8
hjkwon <hjkwon@kismi.kr>
2022-07-20 16:11:44 +0900

    [style]사이드바, 푸터

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Cloud Studio Docs",
-  tagline: "Dinosaurs are cool",
+  tagline: "",
   url: "https://your-docusaurus-test-site.com",
   baseUrl: "/",
   onBrokenLinks: "throw",
@@ -23,8 +23,8 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: "ko",
+    locales: ["ko"],
   },
   plugins: ["docusaurus-plugin-sass"],
 
@@ -40,13 +40,11 @@ const config = {
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
+        // blog: {
+        //   showReadingTime: true,
+        //   editUrl:
+        //     "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        // },
         theme: {
           customCss: require.resolve("./src/scss/_custom.scss"),
         },
@@ -88,12 +86,7 @@ const config = {
             position: "left",
             label: "Documentation",
           },
-          { to: "/blog", label: "Blog", position: "left" },
-          {
-            href: "https://cloudstudio.cloudraw.kr/support/faq  ",
-            label: "FAQ",
-            position: "left",
-          },
+          // { to: "/blog", label: "Blog", position: "left" },
           {
             href: "https://mdxjs.com/",
             label: "Slack community",
@@ -101,7 +94,7 @@ const config = {
           },
           {
             href: "https://cloudstudio.cloudraw.kr/",
-            label: "Drawing Cloud Infra",
+            label: "Demo",
             position: "right",
             className: "btnTrial",
           },
--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -1,29 +1,41 @@
 import React from 'react';
-import Footer from '@theme-original/Footer';
+import {useThemeConfig} from '@docusaurus/theme-common';
+import FooterLinks from '@theme/Footer/Links';
+import FooterLogo from '@theme/Footer/Logo';
+import FooterCopyright from '@theme/Footer/Copyright';
+import FooterLayout from '@theme/Footer/Layout';
 import Styles from '../../scss/_textStyle.module.scss';
+import Link from '@docusaurus/Link';
 
-export default function FooterWrapper(props) {
+
+export default function Footer(props) {
   return (
-    <>
-      <Footer {...props}>
-      </Footer>
-      <section className={Styles.footerWrap}>
-        <section>
-          <div className={Styles.footerWrap_menu}>
-            <a href='https://www.cloudraw.kr/'><h4 className={Styles.footerWrap_menu_text}>About</h4></a>
-            <a href='https://cloudstudio.cloudraw.kr/support/notice'><h4 className={Styles.footerWrap_menu_text}>공지사항</h4></a>
-            <a href='https://cloudstudio.cloudraw.kr/support/policy'><h4 className={Styles.footerWrap_menu_text}>서비스이용약관</h4></a>
-            <a href='https://cloudstudio.cloudraw.kr/payments'><h4 className={Styles.footerWrap_menu_text}>구독정보</h4></a>
+    <footer className={Styles.footer}>
+      <section className={Styles.footer__nav}>
+        <section className={Styles.footer__nav__menu}>
+          <div>
+            <h3>Company</h3>
+            <ul>
+              <li><Link to="https://www.cloudraw.kr/"><h4>About</h4></Link></li>
+              <li><Link to="https://cloudstudio.cloudraw.kr/payments"><h4>Pricing</h4></Link></li>
+              <li><Link to=" "><h4>Policy</h4></Link></li>
+            </ul>
           </div>
           <div>
-            {/* 아이콘 영역 */}
+            <h3>Community</h3>
+            <ul>
+              <li><Link to=" "><h4>Slack</h4></Link></li>
+              <li><Link to=" "><h4>FAQ</h4></Link></li>
+              <li><Link to="https://www.youtube.com/channel/UCHoqm5luu1MvzPQRHVSwH9w"><h4>Youtube</h4></Link></li>
+            </ul>
           </div>
         </section>
-        <section className={Styles.footerWrap_address}>
-          <h4>(주)클라우드로 | 사업자등록번호 : 886-86-02497 | 대표이사 강혜경 | 서울시 강남구 테헤란로 151 (역삼하이츠빌딩) 305</h4>
-          <h4>Copyright ⓒ 2021 Cloudraw. Inc.</h4>
-        </section>
+        <h1>Drawing<p>Your Cloud Infra.</p></h1>
       </section>
-    </> 
+      <section className={Styles.footer__address}>
+        <h5>(주)클라우드로 | 사업자등록번호 : 886-86-02497 | 대표이사 강혜경 | 서울시 강남구 테헤란로 151 (역삼하이츠빌딩) 305</h5>
+        <h5>Copyright ⓒ 2021 Cloudraw. Inc.</h5>
+      </section>
+    </footer>
   );
 }
