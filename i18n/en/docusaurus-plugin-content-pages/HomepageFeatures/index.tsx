e2a873eaeeb5d757ab6626c87e3327cdadd2d2d1
ckkim <ckkim@kismi.kr>
2024-09-24 20:48:47 +0900

    [fix] main image

--- i18n/en/docusaurus-plugin-content-pages/HomepageFeatures/index.tsx
+++ i18n/en/docusaurus-plugin-content-pages/HomepageFeatures/index.tsx
@@ -1,15 +1,16 @@
 import React from "react"
 import styles from "./styles.module.css"
 
-
 export default function HomepageFeatures(): JSX.Element {
     return (
         <section className={styles.features}>
             <div className="container">
                 <div className={styles.mainWrap}>
                     <div className={styles.mainLogo} />
-                    <img style={{ width: "80%", height: "auto" }} src={"./img/cloudstudio_img_en.png"}
-                         alt={"landing"} />
+                    <img style={{ width: "80%", height: "auto" }}
+                         src={"./img/cloudstudio_img_en.png"}
+                         alt={"landing"}
+                    />
                     <div style={{ height: "24px" }} />
                 </div>
             </div>
